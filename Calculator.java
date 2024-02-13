b6e12d543d88d881e4567bb941ba2684a57105df
TrippMenhall <dalton.menhall@quinnipiac.edu>
2024-02-13 17:05:30 -0500

    Ask the user for either addition or subtraction and output the correct option

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,26 @@ public class Calculator{
         System.out.println("Enter 2 Whole Numbers: ");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
+        sc.nextLine();
 
-        int answer = num1 + num2;
-        System.out.println(answer);
+        System.out.println("Which opporation would you like to perform? (addition or subtraction)");
+        String opporation = sc.nextLine();
+
+        int answer = 0;
+
+        switch(opporation){
+            case "addition":
+                answer = num1 + num2;
+                System.out.println(answer);
+                break;
+            case "subtraction":
+                answer = num1 - num2;
+                System.out.println(answer);
+                break;
+            default:
+                System.out.println("Error: Not a valid opporation!");
+        }
+
+        sc.close();
     }
 }
