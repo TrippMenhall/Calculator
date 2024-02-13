dfab608439bdf36ee7d802f40b5ff6b558fa0b9a
TrippMenhall <dalton.menhall@quinnipiac.edu>
2024-02-13 16:56:23 -0500

    Asked user for two numbers and added them together

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Calculator{
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 2 Whole Numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int answer = num1 + num2;
+        System.out.println(answer);
     }
 }
